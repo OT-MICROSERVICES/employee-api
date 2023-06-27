5ca168fde717d283238a1e437327649d68026d9f
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2023-06-27 16:01:07 +0530

    Added complete employee management API's
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

--- model/health.go
+++ model/health.go
@@ -1,10 +1,5 @@
 package model
 
-// HealthCheck is a structure for health check information
-type HealthCheck struct {
-	Message string `json:"message"`
-}
-
 // DetailedHealthCheck is a structure for detailed health check information
 type DetailedHealthCheck struct {
 	Message     string `json:"message"`
--- routes/routes.go
+++ routes/routes.go
@@ -11,4 +11,9 @@ func CreateRouterForEmployee(routerGroup *gin.RouterGroup) {
 	employee := routerGroup.Group("/employee")
 	employee.GET("/health", api.HealthCheckAPI)
 	employee.GET("/health/detail", api.DetailedHealthCheckAPI)
+	employee.POST("/create", api.CreateEmployeeData)
+	employee.GET("/search", api.ReadEmployeeData)
+	employee.GET("/search/all", api.ReadCompleteEmployeesData)
+	employee.GET("/search/location", api.ReadEmployeesLocation)
+	employee.GET("/search/designation", api.ReadEmployeesDesignation)
 }
